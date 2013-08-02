10c075f8db777b63c143966c7b0a5134a081d6e7
Frédéric Harper <fharper@oocz.net>
2013-08-02 09:33:22 -0400

    Changed getElementById with querySelector
    They are faster

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@
     function drawOnCanvas(evt) {
         evt.preventDefault();
 
-        var canvas = document.getElementById("canvas");
+        var canvas = document.querySelector("#canvas");
         var rect = canvas.getBoundingClientRect();
         var context = canvas.getContext("2d");
 
@@ -22,22 +22,22 @@
 
     //Start the drawing on the canvas
     function startDrawOnCanvas() {
-        var canvas = document.getElementById("canvas");
+        var canvas = document.querySelector("#canvas");
         canvas.style.cursor = "pointer";
         canvas.addEventListener("touchmove", drawOnCanvas);
     }
 
     //Stop the drawing on the canvas
     function stopDrawOnCanvas() {
-        var canvas = document.getElementById("canvas");
+        var canvas = document.querySelector("#canvas");
         canvas.style.cursor = "default";
         canvas.removeEventListener("touchmove", drawOnCanvas);
     }
 
     //Add the events at the loading of the page
     window.onload = function() {
-        document.getElementById("canvas").addEventListener("touchstart", startDrawOnCanvas);
-        document.getElementById("canvas").addEventListener("touchend", stopDrawOnCanvas);
+        document.querySelector("#canvas").addEventListener("touchstart", startDrawOnCanvas);
+        document.querySelector("#canvas").addEventListener("touchend", stopDrawOnCanvas);
 
         var canvas = document.getElementById("canvas");
         canvas.width = window.innerWidth;
